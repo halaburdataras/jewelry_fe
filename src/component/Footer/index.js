19eeccf4322e3a188b219c60ce4473563c861a3d
halaburdataras <Taras.Galaburda@outlook.com>
2021-12-08 00:44:19 +0200

    some fixes

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,17 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import CartIcon from "../CartIcon";
 import Logo from "../Logo";
 
 import s from "./style.module.css";
 
 const Footer = ({ setOpenCart, setOpenFollow }) => {
+  const handleOpenFollow = () => {
+    setOpenFollow(true);
+  };
+
+  const handleCloseCart = () => {
+    setOpenCart("close");
+  };
   return (
     <footer>
       <p className={s.copyright}>© 2021 SAPHIRE. All rights reserved</p>
       <div className={`${s.container} ${s.left}`}>
         <Logo />
       </div>
-      <div className={s.container}>Menu</div>
+      <div className={s.container}>
+        <ul className={s.menu}>
+          <li>
+            <Link to="/" onClick={handleCloseCart}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="catalog" onClick={handleCloseCart}>
+              Catalog
+            </Link>
+          </li>
+          <li>FAQ</li>
+          <li onClick={handleOpenFollow}>Follow us</li>
+        </ul>
+      </div>
       <div className={`${s.container} ${s.right}`}>
         <CartIcon setOpenCart={setOpenCart} />
       </div>
